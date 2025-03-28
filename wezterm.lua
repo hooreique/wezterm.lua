17ddb1f13358772c2ca2348fbb3f1da3ed0017c3
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2025-03-28 10:31:12 +0900

    misc

--- wezterm.lua
+++ wezterm.lua
@@ -1,10 +1,6 @@
 local wezterm = require 'wezterm'
 
-local config = {}
-
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
+local config = wezterm.config_builder()
 
 config.default_domain = 'WSL:Ubuntu-24.04'
 
